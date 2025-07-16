8203f5ff45bbb50594071e71feb5162d37d44211
Nicolas Soares <nicksoares3658@gmail.com>
2025-07-16 16:49:55 -0300

    change eventbridge rate

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "eft_twitter_listener" {
 # EVENTBRIDGE DEFINITION
 resource "aws_cloudwatch_event_rule" "eft_twitter_listener_schedule" {
   name = "eft-twitter-listener-schedule"
-  schedule_expression = "rate(8 hours)"
+  schedule_expression = "rate(2 hours)"
   description = "Schedule execution for Lambda function"
 }
 
