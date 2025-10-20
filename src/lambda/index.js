00c6f30410886724daf51d968ece84853080d04c
Nicolas Soares <nicksoares3658@gmail.com>
2025-10-20 13:00:48 -0300

    set prod env

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 
 // MOCKS
-const examplePayload = require('../../mocks/example-payload.json');
+// const examplePayload = require('../../mocks/example-payload.json');
 const messagePresets = require('./messages/messages-presets.json');
 
 // SERVICES
 const axiosService = require('./services/axios');
 
 // REMOVE AFTER TESTS
-(async () => {
-    await handle();
-})();
+// (async () => {
+//     await handle();
+// })();
 
 async function handle() {
   try {
@@ -18,23 +18,25 @@ async function handle() {
     const userName = 'tarkov'
 
     let shouldSendMessage = false;
-    let counter = 0;
+    let auxCounter = 0;
 
-    // const response = await axiosService.getTweets({ userId });
-    const response = examplePayload
-    const last5Posts = response.data.slice(0,5)
-    const last5PostsUrls = last5Posts.map(p => `https://x.com/${userName}/status/${p.id}`) // [0] is the most recent post
+    // const response = examplePayload
+    const response = await axiosService.getTweets({ userId });
+    const last5Posts = response.data.data.slice(0,5)
+    const lastPostsUrls = last5Posts.map(p => `https://x.com/${userName}/status/${p.id}`) // [0] is the most recent post
     const botLastSentMessages = await getBotLastSentMessageOnDiscord();
 
-    console.log('[LOG] Last posts URLs: ', last5PostsUrls);
+    console.log('[LOG] Last posts URLs: ', lastPostsUrls);
     console.log('[LOG] Bot last sent messages: ', botLastSentMessages)
 
-    for (const postUrl of last5PostsUrls) {
-      botLastSentMessages.some(msg => msg.content.includes(postUrl)) ? counter++ : null;
+    for (const postUrl of lastPostsUrls) {
+      botLastSentMessages.some(msg => msg.content.includes(postUrl)) ? auxCounter++ : null;
     }
 
-    if (counter != last5PostsUrls.length) shouldSendMessage = true;
+    if (auxCounter != lastPostsUrls.length) shouldSendMessage = true;
     if (!shouldSendMessage) {
+      console.log('[LOG] Posts already sent');
+
       return {
         statusCode: 200,
         body: JSON.stringify("Posts already sent"),
@@ -47,8 +49,8 @@ async function handle() {
       discordMessagePreset = discordMessagePreset.replace('userId', process.env.GANSO_DISCORD_USER_ID);
     }
     
-    for (const postUrl of last5PostsUrls) {
-      const message = `${discordMessagePreset}\n-----\n${postUrl}`;
+    for (let i = 0; i < lastPostsUrls.length; i++) {
+      const message = (i == 0) ? `${discordMessagePreset}\n-----\n${lastPostsUrls[i]}` : `\n-----\n${lastPostsUrls[i]}`;
       const sendMessageResponse = await axiosService.sendDiscordMessage({ message })
       console.log("[LOG] Message sent: ", { message, sendMessageResponse })
     }
@@ -71,7 +73,7 @@ async function getBotLastSentMessageOnDiscord() {
   const limit = 100;
 
   let oldestMessageId;
-  let botLastSentMessage = [];
+  let botLastSentMessages = [];
   
   try {
     do {
@@ -82,11 +84,11 @@ async function getBotLastSentMessageOnDiscord() {
 
       const lastMessagesSentOnChannel = getDiscordMessagesResponse.data
       
-          botLastSentMessage.push(...lastMessagesSentOnChannel.filter(message => message.author.id === process.env.DISCORD_BOT_ID));
-      if (botLastSentMessage && botLastSentMessage.length >= 5) return botLastSentMessage.slice(0, 5);
+          botLastSentMessages.push(...lastMessagesSentOnChannel.filter(message => message.author.id === process.env.DISCORD_BOT_ID));
+      if (botLastSentMessages && botLastSentMessages.length >= 5) return botLastSentMessages.slice(0, 5);
   
       oldestMessageId = lastMessagesSentOnChannel[limit - 1].id
-    } while (botLastSentMessage.length < 5);
+    } while (botLastSentMessages.length < 5);
   } catch (error) {
     const err = new Error('Error getting bot last sent message on Discord')
           err.details = error
@@ -94,7 +96,7 @@ async function getBotLastSentMessageOnDiscord() {
   }
 }
 
-// exports.handler = async (event) => {
-//   const response = await handle();
-//   return response
-// };
+exports.handler = async (event) => {
+  const response = await handle();
+  return response
+};
